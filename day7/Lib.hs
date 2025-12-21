13676ba59f99b09951e8ea22c9dd39b702f088b6
0xbeedee <81985269+0xbeedee@users.noreply.github.com>
2025-12-21 11:49:44 +0100

    make code more idiomatic and general

--- day7/Lib.hs
+++ day7/Lib.hs
@@ -5,15 +5,20 @@ import Data.Set (Set)
 import Data.List (elemIndex)
 import Data.Maybe (fromJust)
 
+-- Checks whether a character is a splitter
+isSplitter :: Char -> Bool
+isSplitter = (== '^')
+
 -- Processes each row, updating the number of beams if they hit the splitters
 processRow :: (Set Int, Int) -> String -> (Set Int, Int)
 processRow (beams, splits) row =
     Set.foldl' processBeam (Set.empty, splits) beams
     where
-        processBeam (newBeams, splitCount) col =
-            if col >= 0 && col < length row && row !! col == '^' 
-                then (Set.insert (col + 1) (Set.insert (col - 1) newBeams), splitCount + 1)
-            else
+        rowLen = length row
+        processBeam (newBeams, splitCount) col 
+            | col >= 0 && col < rowLen && isSplitter (row !! col) = 
+                (Set.insert (col + 1) (Set.insert (col - 1) newBeams), splitCount + 1)
+            | otherwise =
                 (Set.insert col newBeams, splitCount)
 
 -- Simulates the progression of the beam through the manifold
