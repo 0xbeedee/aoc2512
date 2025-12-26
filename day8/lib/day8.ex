5e5a665960699deece4ad990251cd0e1a6082d72
0xbeedee <81985269+0xbeedee@users.noreply.github.com>
2025-12-26 15:46:02 +0100

    add simple solution to phase 1 of day 8

--- day8/lib/day8.ex
+++ day8/lib/day8.ex
@@ -4,15 +4,44 @@ defmodule Day8 do
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Day8.hello()
-      :world
-
+  Calculates the Euclidean distance between two 3D points.
   """
-  def hello do
-    :world
+  def distance({x1, y1, z1}, {x2, y2, z2}) do
+    :math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) ** 2)
+  end
+
+  @doc """
+  Calculates all pairwise distances between the coordinates.
+  """
+  def all_distances(coords) do
+    for {point1, i} <- Enum.with_index(coords),
+        {point2, j} <- Enum.with_index(coords),
+        # distances are symmetric, can save computation by only computing the upper triangle
+        i < j do
+      # keep track of the nodes as well
+      {distance(point1, point2), i, j}
+    end
+  end
+
+  @doc """
+  Constructs the connected components, given the 1000 pairs of jboxes which are closest together.
+  """
+  def build_components(edges, num_nodes) do
+    # init each node to its own connected component
+    init = for i <- 0..(num_nodes - 1), do: MapSet.new([i])
+
+    Enum.reduce(edges, init, fn {_dist, i, j}, components ->
+      set_with_i = Enum.find(components, fn set -> MapSet.member?(set, i) end)
+      set_with_j = Enum.find(components, fn set -> MapSet.member?(set, j) end)
+
+      # different components => merge
+      if(set_with_i != set_with_j) do
+        merged = MapSet.union(set_with_i, set_with_j)
+        remaining = Enum.filter(components, fn set -> set != set_with_i and set != set_with_j end)
+        [merged | remaining]
+      else
+        components
+      end
+    end)
   end
 end
--- day8/lib/main.ex
+++ day8/lib/main.ex
@@ -2,15 +2,40 @@ defmodule Day8.Main do
   @moduledoc """
   Main entry point for Day 8 solution
   """
+  import Day8
 
   def main do
-    content = File.read!("jboxes.txt")
+    coords =
+      File.read!("jboxes.txt")
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn line ->
+        line
+        |> String.split(",")
+        |> Enum.map(&String.to_integer/1)
+        |> List.to_tuple()
+      end)
 
-    # TODO: Implement solution
-    result_phase1 = 0
-    result_phase2 = 0
+    edges =
+      coords
+      |> all_distances()
+      |> Enum.sort()
+      # take the 1000 edges with the shortest distances
+      |> Enum.take(1000)
+
+    num_nodes = length(coords)
+
+    result_phase1 =
+      build_components(edges, num_nodes)
+      # take the three biggest connected components
+      |> Enum.map(&MapSet.size/1)
+      |> Enum.sort(:desc)
+      |> Enum.take(3)
+      # multiply their sizes
+      |> Enum.product()
 
     IO.puts("[PHASE 1] Result: #{result_phase1}")
+
+    result_phase2 = 0
     IO.puts("[PHASE 2] Result: #{result_phase2}")
   end
 end
